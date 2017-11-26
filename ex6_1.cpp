ed3dd28908ac30dca94922947c7e8aebe9d9b046
yuelanzhe <32035895+yuelanzhe@users.noreply.github.com>
2017-11-26 22:39:08 +0800

    Add files via upload

---
+++ ex6_1.cpp
@@ -0,0 +1,41 @@
+#include<iostream.h>
+#define N 100
+void main()
+{
+	int a[N],n,i,t;
+	 int max,min,j,k;
+    cout<<"请输入数组的大小：";
+	cin>>n;
+	cout<<"请依次输入"<<n<<"个数组元素的值"<<endl;
+	for(i=0;i<n;i++)
+	{
+		cout<<"Enter a["<<i<<"]:";
+		cin>>a[i];
+	}
+	max=min=a[0];
+	j=k=0;
+	for(i=0;i<n;i++)
+	{
+		if(a[i]>max)
+		{
+			max=a[i]; 
+			j=i;
+		}
+		else if(a[i]<min)
+		{
+			min=a[i]; 
+			k=i;
+		}
+	}
+	if(j!=k)
+	{
+		t=a[j];
+		a[j]=a[k];
+		a[k]=t;
+	}
+	cout<<"交换后的数组：";
+	for(i=0;i<n;i++)
+		cout<<a[i]<<" ";
+}
+
+	
